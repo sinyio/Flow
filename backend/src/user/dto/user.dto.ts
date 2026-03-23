d8e733a1427ea4434d05683b11464874bd59732e
Ilya Sinyakin <arqu1t@mail.ru>
2026-03-23 20:44:10 +0300

    [BACK] Микро правка

--- backend/src/user/dto/user.dto.ts
+++ backend/src/user/dto/user.dto.ts
@@ -4,4 +4,5 @@ export const getUserResponse = (user: User) => ({
     id: user.id,
     fullName: `${user.firstName} ${user.lastName}`,
     photo: user.photo ?? null,
+    deletedAt: user.deletedAt,
 })
