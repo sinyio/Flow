e033ada6f1555c3927e024eeeb51c58c5433454b
sinyio <iosinyakin@gmail.com>
2026-04-20 15:28:06 +0300

    Микро фикс

--- backend/src/media/media.module.ts
+++ backend/src/media/media.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common'
 import { MediaController } from './media.controller'
 import { MediaService } from './media.service'
 import { UserModule } from '../user/user.module'
+import { S3Service } from '../s3/s3.service'
 
 @Module({
   imports: [UserModule],
   controllers: [MediaController],
-  providers: [MediaService],
+  providers: [MediaService, S3Service],
 })
 export class MediaModule {}
 
