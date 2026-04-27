b052b7b0ca691250d959d6661fe050add29ab71d
sinyio <iosinyakin@gmail.com>
2026-04-27 16:41:07 +0300

    [BACK] Fix

--- backend/src/user/dto/user.dto.ts
+++ backend/src/user/dto/user.dto.ts
@@ -2,7 +2,9 @@ import { User } from "@prisma/client";
 
 export const getUserResponse = (user: User) => ({
     id: user.id,
-    fullName: `${user.firstName} ${user.lastName}`,
+    fullName: user.firstName && user.lastName
+      ? `${user.firstName} ${user.lastName}`
+      : user.firstName ?? user.lastName ?? 'Неизвестный пользователь',
     photo: user.photo ?? null,
     deletedAt: user.deletedAt,
     courierRating: user.courierRating,
