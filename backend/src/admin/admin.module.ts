0b3dc89d6f249369cc6f53b2e183f609f97ad2c5
sinyio <iosinyakin@gmail.com>
2026-04-28 14:59:31 +0300

    [BACK] Fix

--- backend/src/admin/admin.module.ts
+++ backend/src/admin/admin.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common'
 
 import { AuthGuard } from '../auth/guards/auth.guard'
 import { UserService } from '../user/user.service'
+import { S3Service } from '../s3/s3.service'
 import { AdminController } from './admin.controller'
 import { AdminService } from './admin.service'
 import { AdminGuard } from './guards/admin.guard'
 
 @Module({
   controllers: [AdminController],
-  providers: [AdminService, AdminGuard, AuthGuard, UserService],
+  providers: [AdminService, AdminGuard, AuthGuard, UserService, S3Service],
 })
 export class AdminModule {}
